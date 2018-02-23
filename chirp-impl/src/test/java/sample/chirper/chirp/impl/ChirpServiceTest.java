6cea6300fcffa61b6bc9ed30eb2f801bb3abd9cf
Ignasi Marimon-Clos <ignasi@lightbend.com>
2018-02-23 11:56:48 +0100

    Reduce occurrences of 'magic number' and remove code duplication

--- chirp-impl/src/test/java/sample/chirper/chirp/impl/ChirpServiceTest.java
+++ chirp-impl/src/test/java/sample/chirper/chirp/impl/ChirpServiceTest.java
@@ -33,8 +33,9 @@ public class ChirpServiceTest {
     // Creating a new persistent entity is slow in some runtime environment (e.g. free tier Travis) causing the
     // tests to fail on timeout. First time we invoke a call that requires a persistent entity we will
     // wait a bit longer.
-    private static int firstRequestOnEntityTimeout;
-    private static final int secondRequestOnEntityTimeout = 1;
+    private static int persistentEntityDefault;
+    private static final int probeRequestTimeout = 10;
+    private static final int serviceInvocationTimeout = 3;
 
     @BeforeClass
     public static void setUp() throws InterruptedException, ExecutionException, TimeoutException {
@@ -46,9 +47,8 @@ public class ChirpServiceTest {
         LiveChirpsRequest request = new LiveChirpsRequest(TreePVector.<String>empty().plus("usr1").plus("usr2"));
         chirpService.getLiveChirps().invoke(request).toCompletableFuture().get(10, SECONDS);
 
-        // First time a given PE is requested it may take longer to respond. Let's run the tests with
-        // timeouts equivalent to production settings _plus_ one second.
-        firstRequestOnEntityTimeout = 1 + (int) server.app().config().getDuration("lagom.persistence.ask-timeout", SECONDS);
+        // Let's run the tests with timeouts equivalent to production settings _plus_ one second.
+        persistentEntityDefault = 1 + (int) server.app().config().getDuration("lagom.persistence.ask-timeout", SECONDS);
     }
 
     @AfterClass
@@ -61,25 +61,25 @@ public class ChirpServiceTest {
     public void shouldPublishChirpsToSubscribers() throws Exception {
         ChirpService chirpService = server.client(ChirpService.class);
         LiveChirpsRequest request = new LiveChirpsRequest(TreePVector.<String>empty().plus("usr1").plus("usr2"));
-        Source<Chirp, ?> chirps1 = chirpService.getLiveChirps().invoke(request).toCompletableFuture().get(1, SECONDS);
+        Source<Chirp, ?> chirps1 = chirpService.getLiveChirps().invoke(request).toCompletableFuture().get(serviceInvocationTimeout, SECONDS);
         Probe<Chirp> probe1 = chirps1.runWith(TestSink.probe(server.system()), server.materializer());
-        probe1.request(10);
-        Source<Chirp, ?> chirps2 = chirpService.getLiveChirps().invoke(request).toCompletableFuture().get(1, SECONDS);
+        probe1.request(probeRequestTimeout);
+        Source<Chirp, ?> chirps2 = chirpService.getLiveChirps().invoke(request).toCompletableFuture().get(serviceInvocationTimeout, SECONDS);
         Probe<Chirp> probe2 = chirps2.runWith(TestSink.probe(server.system()), server.materializer());
-        probe2.request(10);
+        probe2.request(probeRequestTimeout);
 
         Chirp chirp1 = new Chirp("usr1", "hello 1");
-        chirpService.addChirp("usr1").invoke(chirp1).toCompletableFuture().get(firstRequestOnEntityTimeout, SECONDS);
+        addChirp(chirpService, chirp1);
         probe1.expectNext(chirp1);
         probe2.expectNext(chirp1);
 
         Chirp chirp2 = new Chirp("usr1", "hello 2");
-        chirpService.addChirp("usr1").invoke(chirp2).toCompletableFuture().get(secondRequestOnEntityTimeout, SECONDS);
+        addChirp(chirpService, chirp2);
         probe1.expectNext(chirp2);
         probe2.expectNext(chirp2);
 
         Chirp chirp3 = new Chirp("usr2", "hello 3");
-        chirpService.addChirp("usr2").invoke(chirp3).toCompletableFuture().get(firstRequestOnEntityTimeout, SECONDS);
+        addChirp(chirpService, chirp3);
         probe1.expectNext(chirp3);
         probe2.expectNext(chirp3);
 
@@ -92,21 +92,21 @@ public class ChirpServiceTest {
         ChirpService chirpService = server.client(ChirpService.class);
 
         Chirp chirp1 = new Chirp("usr3", "hi 1");
-        chirpService.addChirp("usr3").invoke(chirp1).toCompletableFuture().get(firstRequestOnEntityTimeout, SECONDS);
+        addChirp(chirpService, chirp1);
 
         Chirp chirp2 = new Chirp("usr4", "hi 2");
-        chirpService.addChirp("usr4").invoke(chirp2).toCompletableFuture().get(firstRequestOnEntityTimeout, SECONDS);
+        addChirp(chirpService, chirp2);
 
         LiveChirpsRequest request = new LiveChirpsRequest(TreePVector.<String>empty().plus("usr3").plus("usr4"));
 
         eventually(FiniteDuration.create(10, SECONDS), () -> {
-            Source<Chirp, ?> chirps = chirpService.getLiveChirps().invoke(request).toCompletableFuture().get(3, SECONDS);
+            Source<Chirp, ?> chirps = chirpService.getLiveChirps().invoke(request).toCompletableFuture().get(serviceInvocationTimeout, SECONDS);
             Probe<Chirp> probe = chirps.runWith(TestSink.probe(server.system()), server.materializer());
-            probe.request(10);
+            probe.request(probeRequestTimeout);
             probe.expectNextUnordered(chirp1, chirp2);
 
             Chirp chirp3 = new Chirp("usr4", "hi 3");
-            chirpService.addChirp("usr4").invoke(chirp3).toCompletableFuture().get(3, SECONDS);
+            addChirp(chirpService, chirp3);
             probe.expectNext(chirp3);
 
             probe.cancel();
@@ -118,20 +118,24 @@ public class ChirpServiceTest {
         ChirpService chirpService = server.client(ChirpService.class);
 
         Chirp chirp1 = new Chirp("usr5", "msg 1");
-        chirpService.addChirp("usr5").invoke(chirp1).toCompletableFuture().get(firstRequestOnEntityTimeout, SECONDS);
+        addChirp(chirpService, chirp1);
 
         Chirp chirp2 = new Chirp("usr6", "msg 2");
-        chirpService.addChirp("usr6").invoke(chirp2).toCompletableFuture().get(firstRequestOnEntityTimeout, SECONDS);
+        addChirp(chirpService, chirp2);
 
         HistoricalChirpsRequest request = new HistoricalChirpsRequest(Instant.now().minusSeconds(20),
             TreePVector.<String>empty().plus("usr5").plus("usr6"));
 
         eventually(FiniteDuration.create(10, SECONDS), () -> {
-            Source<Chirp, ?> chirps = chirpService.getHistoricalChirps().invoke(request).toCompletableFuture().get(3, SECONDS);
+            Source<Chirp, ?> chirps = chirpService.getHistoricalChirps().invoke(request).toCompletableFuture().get(serviceInvocationTimeout, SECONDS);
             Probe<Chirp> probe = chirps.runWith(TestSink.probe(server.system()), server.materializer());
-            probe.request(10);
+            probe.request(probeRequestTimeout);
             probe.expectNextUnordered(chirp1, chirp2);
             probe.expectComplete();
         });
+    }
+
+    private void addChirp(ChirpService chirpService, Chirp chirp) throws InterruptedException, ExecutionException, TimeoutException {
+        chirpService.addChirp(chirp.userId).invoke(chirp).toCompletableFuture().get(persistentEntityDefault, SECONDS);
     }
 }
